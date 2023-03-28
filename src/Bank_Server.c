13f49d9e63dcaed45592e1a9561546501f28abbf
notbrayton <brayton.rude@gmail.com>
2023-03-28 10:15:18 -0500

    command line input work

--- src/Bank_Server.c
+++ src/Bank_Server.c
@@ -7,15 +7,22 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char * argv) {
+int main(int argc, char *argv[]) {
     // Parse Command line input
     if (argc != 4) {
-        printf("ERROR: Command line input invalid, required format:\n\t$ appserver <# of worker threads> <# of account> <output file>\n");
+        printf("ERROR: Command line input invalid, required format:\n\t$ server <# of worker threads> <# of account> <output file>\n");
         return 0;
     }
 
-    
+    int numWThreads = atoi(argv[1]);
+    int numAccounts = atoi(argv[2]);
+    char * outFName = argv[3];
+
+    printf("Creating %d Worker Threads...\n", numWThreads);
+    printf("Creating %d Accounts...\n", numAccounts);
+    printf("Creating Output File <%s.txt>...\n", outFName);
 
     // Initialize progam based on command line input
         // Setup output file
