3ed4a651d98eacbb5de05c054795229550ca59ac
rude87 <rude87@co2061-12.ece.iastate.edu>
2023-03-28 11:49:06 -0500

    yadadadada

--- src/Bank_Server.c
+++ src/Bank_Server.c
@@ -8,7 +8,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "Bank.h"
+
+struct trans {      // Structure for a transaction pair
+    int acc_id;     // Account ID
+    int amount;     // amount to be added, could be positive or negative
+};
+
+struct request {                        // Structure for a request object    
+    struct request * next;              // pointer to the next request in the list
+    int request_id;                     // request ID assigned by the main thread
+    int check_acc_id;                   // account ID for a CHECK request
+    struct trans * transactions;        // array of transaction data
+    int num_trans;                      // number of accounts in this transaction
+    struct timeval starttime, endtime;  // starttime and endtime for TIME
+};
+
+struct queue {                      // Structure for a queue 
+    struct request * head, * tail;  // head and tail of the list
+    int num_jobs;                   // number of jobs currently in queue
+};
+
+
+
 
 int main(int argc, char *argv[]) {
     // Command Line Input Error Handling
@@ -20,22 +43,30 @@ int main(int argc, char *argv[]) {
     int numWThreads = atoi(argv[1]);
     int numAccounts = atoi(argv[2]);
     char * outFName = argv[3];
+    
+    printf("Creating Output File <%s>...\n", outFName);
+    FILE *fp;
+    fp = fopen(outFName, "w");
+    fprintf(fp, "Hello, File!\n");
+    fclose(fp);
 
     printf("Creating %d Worker Threads...\n", numWThreads);
+    if (numWThreads < 1) {
+        printf("ERROR: Invalid worker thread amount, must be at least 1.\n");
+        return 0;
+    }
 
     printf("Creating %d Accounts...\n", numAccounts);
     if (!initialize_accounts(numAccounts)) {
         printf("ERROR: Account creation failed.\n");
         return 0;
     }
-    
-    printf("Creating Output File <%s.txt>...\n", outFName);
 
-    // Initialize progam based on command line input
-        // Setup output file
-        // Create the fixed number of bank accounts
-            // Utilize initialize_accounts(int n); to create accounts
-        // Create specified number of worker threads
+    struct timeval time;
+    gettimeofday(&time, NULL);
+    printf("TIME %ld.%06.ld\n", time.tv_sec, time.tv_usec);
+    
+    // Create specified number of worker threads
          
     // Begin Program Loop
         // Waits for request
