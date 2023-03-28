1aecc933a3ab80d2e05d52f2104238bf3a6774a1
rude87 <rude87@co1313-04.ece.iastate.edu>
2023-03-28 13:32:00 -0500

    blah

--- src/Bank_Server.c
+++ src/Bank_Server.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
 #include "Bank.h"
 
 struct trans {      // Structure for a transaction pair
@@ -30,8 +31,8 @@ struct queue {                      // Structure for a queue
     int num_jobs;                   // number of jobs currently in queue
 };
 
-
-
+// Function Declaration
+void* dummy_thread(FILE * fp);
 
 int main(int argc, char *argv[]) {
     // Command Line Input Error Handling
@@ -44,29 +45,34 @@ int main(int argc, char *argv[]) {
     int numAccounts = atoi(argv[2]);
     char * outFName = argv[3];
     
+    // Setting Up Output File
     printf("Creating Output File <%s>...\n", outFName);
     FILE *fp;
     fp = fopen(outFName, "w");
     fprintf(fp, "Hello, File!\n");
     fclose(fp);
 
+    // Creating Worker Threads
     printf("Creating %d Worker Threads...\n", numWThreads);
     if (numWThreads < 1) {
         printf("ERROR: Invalid worker thread amount, must be at least 1.\n");
         return 0;
+    } else {
+        // Create Worker Threads
     }
 
+    // Initializing Accounts
     printf("Creating %d Accounts...\n", numAccounts);
     if (!initialize_accounts(numAccounts)) {
         printf("ERROR: Account creation failed.\n");
         return 0;
     }
 
+/*
     struct timeval time;
     gettimeofday(&time, NULL);
     printf("TIME %ld.%06.ld\n", time.tv_sec, time.tv_usec);
-    
-    // Create specified number of worker threads
+*/    
          
     // Begin Program Loop
         // Waits for request
@@ -74,5 +80,18 @@ int main(int argc, char *argv[]) {
         // Adds request to request queue
             // Unless the request is an END request
 
+    // Creates a pthread variable
+    pthread_t d1;
+    
+    // Creates a new thread with a storage location d1, and start routine dummy_thread
+    pthread_create(&d1, NULL, dummy_thread, NULL);
+
+    // Pauses the calling thread main()'s execution until the thread under d1 is terminated
+    pthread_join(d1, NULL);
+
     return 0;
+}
+
+void* dummy_thread(FILE * fp) {
+    fprintf(fp, "Hello from dummy thread.");
 }
